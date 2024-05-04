c3609aced4421390e1c2e853422b8dd17c999efe
Ayush Chopra <86606259+BrownCoderAyush@users.noreply.github.com>
2024-05-04 13:10:59 +0530

    crated session model and migration

---
+++ src/models/session.js
@@ -0,0 +1,46 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Session extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Session.belongsTo(models.User,{
+        foreignKey : 'user_id',
+        onDelete :'CASCADE',
+        as:'user_has_a_session'
+      })
+    }
+  }
+  Session.init({
+    user_id: {     
+      type : DataTypes.INTEGER ,
+    },
+    time:{
+      type : DataTypes.DATE,
+      allowNull : false
+    },
+    price:{
+      type : DataTypes.INTEGER,
+      allowNull : false,
+      defaultValue : 0 
+    },
+    typeOfSession : {
+      type : DataTypes.ENUM("1-1","1-m","n:m"),
+      default : "1-1",
+      allowNull : false
+    }
+
+  }, {
+    sequelize,
+    modelName: 'Session',
+    timestamps : false 
+  });
+  return Session;
+};
