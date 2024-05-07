a81438f603339ab0abf34f095590803dfc5ff0fe
Riktam Santra <32616925+Riktam-Santra@users.noreply.github.com>
2024-05-07 20:51:39 +0530

    added if condition to allow mentors to get mentor profile only

--- src/controllers/userProfile.controller.js
+++ src/controllers/userProfile.controller.js
@@ -29,36 +29,36 @@ const getMentorProfile = async (req, res) => {
         const userId = req.params.userId;
         const user = await UserService.getUser({ id: userId });
         const role = await RoleService.getRoleNameById(user.dataValues.roleId);
-        // if (role.dataValues.roleName !== "Mentor") {
-        //     res.status(400);
-        //     //need to discuss what to send here instead of this json
-        //     //if the requested user is not a mentor
-        //     return res.json({ 'error': 'user is not a mentor' });
-        // }
-        // else {
-        const reviews = await ReviewService.getReviewsMentor(userId)
-        const reviewData = new Array();
-        for (const x of reviews) {
-            const userData = await getUserDetails(x.dataValues.userId);
-            const reviewerData = await getUserDetails(x.dataValues.reviewerId);
-            reviewData.push({
-                id: x.dataValues.id,
-                user: userData,
-                reviewer: reviewerData,
-                content: x.dataValues.content,
-                createdAt: x.dataValues.createdAt
+        if (role.dataValues.roleName !== "Mentor") {
+            res.status(400);
+            //need to discuss what to send here instead of this json
+            //if the requested user is not a mentor
+            return res.json({ 'error': 'user is not a mentor' });
+        }
+        else {
+            const reviews = await ReviewService.getReviewsMentor(userId)
+            const reviewData = new Array();
+            for (const x of reviews) {
+                const userData = await getUserDetails(x.dataValues.userId);
+                const reviewerData = await getUserDetails(x.dataValues.reviewerId);
+                reviewData.push({
+                    id: x.dataValues.id,
+                    user: userData,
+                    reviewer: reviewerData,
+                    content: x.dataValues.content,
+                    createdAt: x.dataValues.createdAt
+                });
+            }
+            console.log(reviewData);
+            // console.log(reviews);
+            return res.json({
+                nxtGenStatus: 0,
+                picture: user.dataValues.picture,
+                username: user.dataValues.username,
+                role: role.dataValues.roleName,
+                reviews: reviewData
             });
         }
-        console.log(reviewData);
-        // console.log(reviews);
-        return res.json({
-            nxtGenStatus: 0,
-            picture: user.dataValues.picture,
-            username: user.dataValues.username,
-            role: role.dataValues.roleName,
-            reviews: reviewData
-        });
-        // }
     } catch (err) {
         res.json({
             nxtGenStatus: 1,
