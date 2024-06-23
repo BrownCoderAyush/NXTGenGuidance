c3bcb257f3385ccf2295da19b359b0186941cd01
Ayush Chopra <86606259+BrownCoderAyush@users.noreply.github.com>
2024-06-23 13:15:06 +0530

    removed password field and added default username functionality

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,11 +1,15 @@
+const devLogger = require("../config/devLogger");
 const {User,Role}=require("../models/index");
 class UserService{
 
     static async getUser(queryParams){
 
         const user = await User.findOne({
-            where : queryParams
-        })
+            where: queryParams,
+            raw: true // This option returns plain JSON objects instead of Sequelize model instances
+        });
+        console.log(user);
+        devLogger().info(`data ${user}`);
         return user;
     }
 
